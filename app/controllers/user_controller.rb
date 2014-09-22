22b51c6bdd1eace45e7068f9e50e0e238c421be5
RaSiel93 <aleksandrpoplawskiy@gmail.com>
2014-09-23 02:31:37 +0300

    correct url for redirect_url (vk)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
     require 'json'
 
     if params[:code].present?
-      response = JSON.parse Net::HTTP.get(URI.parse("https://oauth.vk.com/access_token?client_id=#{Settings.client_id}&client_secret=#{Settings.client_secret}&code=#{params[:code]}&redirect_uri=#{Rails.root}/#{Settings.authorization_path}"))
+      response = JSON.parse Net::HTTP.get(URI.parse("https://oauth.vk.com/access_token?client_id=#{Settings.client_id}&client_secret=#{Settings.client_secret}&code=#{params[:code]}&redirect_uri=#{authorization_url}"))
       User.create(access_token: response['access_token'])
     end
     redirect_to root_path
